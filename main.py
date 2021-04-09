6eae678bf9b26823b5caeed1f0dfc1fa5ed920c2
Sagar Shinde <sagar@fice.in>
2021-04-09 18:58:53 +0530

    Added main.py and add.py
    Addition Functionality

---
+++ main.py
@@ -0,0 +1,12 @@
+import add
+
+input_num1 = input("Give the first number:")
+input_num1 = int(input_num1)
+
+input_num2 = input("Give the second number:")
+input_num2 = int(input_num2)
+
+#Calling add
+
+res_add = add.add(input_num1,input_num2)
+print("The result of the addition is :",res_add)
